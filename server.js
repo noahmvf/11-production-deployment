e914b80acd1d7879ed48e4de63ea77c0c0372122
Noah Israel <noahmvf@gmail.com>
2018-04-24 11:21:11 -0700

    starter code fixed

---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const cors = require('cors');
+const pg = require('pg');
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT;
+const CLIENT_URL = process.env.CLIENT_URL;
+
+const client = new pg.Client (process.env.DATABASE_URL);
